6cbd3581156dad2b679f21187bc98db5ac52e334
briecodes <hello@brie.work>
2018-06-08 10:36:07 -0400

    commented-out state and functions in filter deleted.

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -5,19 +5,8 @@ class Filter extends Component {
     super();
 
     this.state = {
-      // filters: []
     };
   }
-
-  // componentWillMount() {
-  //   this.fetchFilters();
-  // }
-
-  // fetchFilters = () => {
-  //   fetch('/api/fruit_types')
-  //     .then(response => response.json())
-  //     .then(filters => this.setState({ filters }));
-  // }
 
   render() {
     return (
